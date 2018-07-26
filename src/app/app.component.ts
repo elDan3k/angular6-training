abf5f5c1d0b38a15a6de5ae6643d2ffb499a5e45
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-07-26 13:44:43 +0200

    Books form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,46 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  message = 'Hello World';
+  books = [
+    {
+      id: 1,
+      title: 'Angular in action',
+      authors: 'Jan Kowalski',
+      category: '#00ff00',
+      bestseller: true
+    },
+    {
+      id: 2,
+      title: 'React in action',
+      authors: 'Marek Nowak',
+      category: '#00ff00',
+      bestseller: false
+    }
+  ];
+  titleSize = 24;
 
-  sayHello() {
-    this.message = 'Hello Angular';
+  get authorsStyle() {
+    return {
+      color: 'gray',
+      letterSpacing: '2px'
+    };
+  }
+
+  selectedBook = null;
+  editedBook = null;
+
+  select(book) {
+    this.selectedBook = book;
+    this.editedBook = Object.assign({}, book);
+  }
+
+  save() {
+    Object.assign(this.selectedBook, this.editedBook);
+    this.reset();
+  }
+
+  reset() {
+    this.selectedBook = null;
+    this.editedBook = null;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 
-import {AppComponent} from './app.component';
-import { DateComponent } from './date/date.component';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DateComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
