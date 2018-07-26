a738bc2bf8b77793212d735096d5098f6401ac6e
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2018-07-26 12:44:19 +0200

    Date component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import { DateComponent } from './date/date.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DateComponent
   ],
   imports: [
     BrowserModule
